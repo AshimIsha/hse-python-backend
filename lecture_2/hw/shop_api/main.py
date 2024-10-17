4655be257e346c478f0edb0aed7c08cb1709006a
AshimIsha <krz.m@ya.ru>
2024-10-17 22:52:10 +0300

    No module named 'routers'

--- lecture_2/hw/shop_api/main.py
+++ lecture_2/hw/shop_api/main.py
@@ -1,7 +1,6 @@
 
 from fastapi import FastAPI
-from routers import shop_router
+from .routers import shop_router
 app = FastAPI()
 
 app.include_router(shop_router)
-#app.include_router(router = item_router)
