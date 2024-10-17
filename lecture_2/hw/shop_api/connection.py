a8b2185b7b3156100324d0d0ac640692996af807
AshimIsha <krz.m@ya.ru>
2024-10-17 23:04:40 +0300

    No module named 'database'

--- lecture_2/hw/shop_api/connection.py
+++ lecture_2/hw/shop_api/connection.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from database import Base
+from .database import Base
 
 engine = create_engine("sqlite:///./shop.db", connect_args={"check_same_thread": False})
 Session = sessionmaker(autocommit = False, autoflush = False, bind = engine)
